48161d5125b12474f10fb02bf43e3b3d2247db1f
Icaro <icaro.moreira@protonmail.com>
2020-06-09 18:06:57 -0300

    Implementaçao das API's do IBGE

--- web/src/pages/createpoint/index.tsx
+++ web/src/pages/createpoint/index.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, ChangeEvent} from "react"
 import "./Styles.css"
 import logo from "../../assets/logo.svg"
 import {  Link  } from "react-router-dom"
 import {  Map, TileLayer, Marker} from "react-leaflet"
 import Api from "../../services/api"
 import Axios from "axios"
-
 // Importando o modulo do feather icons
 import {  FiArrowLeft  } from "react-icons/fi"
 
@@ -21,11 +20,19 @@ interface IBGEUFresponse {
     sigla: string
 }
 
+interface IBGECityresponse {
+    nome: string
+}
+
 
 function CreatePoint() {
 
     const [items, setItems] = useState<Item[]>([])
     const [ufs, setUfs] = useState<string[]>([])
+    const [cities, setCities] = useState<string[]>([])
+    
+    // Estado que armazena qual UF o usuário selecionou
+    const [selectedUF , setSelectedUF] = useState("0")
 
     // Função para armazenar o estado
     useEffect(() => {
@@ -47,7 +54,32 @@ function CreatePoint() {
             // Setando o estado dos UFs com os dados a API do IBGE
             setUfs(ufInitials)
         })
-    },[])
+    }, [])
+
+    // Carregar as cidades sempre que a UF mudar
+    useEffect(() => {
+
+        if(selectedUF === "0") {
+            return
+        }
+
+        // API do IBGE para listar as cidades por estado
+        Axios.get<IBGECityresponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`).then(response => {
+
+            const cityNames = response.data.map(city => city.nome)
+        
+            // Setando o estado dos UFs com os dados a API do IBGE
+            setCities(cityNames)
+        })
+    }, [selectedUF])
+
+    // <HTMLSelectElement> informa a alteração de um evento HTML select event
+    function handleSelectedUF(event: ChangeEvent<HTMLSelectElement>) {
+        
+        const uf = event.target.value
+        setSelectedUF(uf)
+
+    }
 
 
     return (
@@ -59,7 +91,6 @@ function CreatePoint() {
                     <FiArrowLeft/>
                 </Link>
             </header>
-
             <form action="">
                 <h1>Cadastro do ponto de coleta</h1>
                 <fieldset>
@@ -79,10 +110,8 @@ function CreatePoint() {
                             <label htmlFor="whatsapp">Whatsapp</label>
                             <input type="text" name="whatsapp" id="whatsapp"/>
                         </div>
-
                     </div>
                 </fieldset>
-
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
@@ -95,11 +124,10 @@ function CreatePoint() {
                         />
                         <Marker position={[-27.2092052, -49.6401092]}></Marker>
                     </Map>
-
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado</label>
-                            <select name="uf" id="uf">
+                            <select name="uf" id="uf" value={selectedUF} onChange={handleSelectedUF}>
                                 <option value="0">Selecione um estado</option>
                                 {ufs.map(uf => (
                                     <option key={uf} value={uf}>{uf}</option>
@@ -110,11 +138,13 @@ function CreatePoint() {
                             <label htmlFor="city">Cidade</label>
                             <select name="city" id="city">
                                 <option value="0">Selecione uma cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
                             </select>
                         </div>                      
                     </div>
                 </fieldset>
-
                 <fieldset>
                     <legend>
                         <h2>Itens de coletas</h2>
